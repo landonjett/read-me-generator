524182d549f882cef3259da9b83f91784d4ac659
landonjett <landonjett@gmail.com>
2024-01-09 15:24:14 -0600

    added array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,55 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use the application?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can others contribute to the project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide any test instructions:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:'
+    }
+];
+;
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -7,10 +7,26 @@ function renderLicenseBadge(license) {
   return `![License: ${license}](https://img.shields.io/badge/license-${encodeURI(license.replace(' ', '%20'))}-blue.svg)`;
 }
 
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license || license === 'None') {
+      return '';
+  }
+  switch(license) {
+      case 'MIT':
+          return '[MIT License](https://opensource.org/licenses/MIT)';
+      case 'GPLv3':
+          return '[GPLv3 License](https://www.gnu.org/licenses/gpl-3.0)';
+      case 'Apache 2.0':
+          return '[Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)';
+      case 'BSD 3-Clause':
+          return '[BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause)';
+      default:
+          return '';
+  }
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
